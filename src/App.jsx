7bedbc089e8c50396d0ec952ae1e2030a4da9e40
marufsiyam91 <marufsiyam9123@gmail.com>
2024-05-29 15:02:58 +0600

    taskly task management app initialized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Taskly</h1>
+      <p>A comprehensive task management application designed to help users organize and streamline their daily activities with ease.</p>
+    </>
+  )
+}
+
+export default App

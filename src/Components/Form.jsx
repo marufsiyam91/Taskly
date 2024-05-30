c9f18088bf2927cf49df804b8c61236bd29d4ca4
marufsiyam91 <marufsiyam9123@gmail.com>
2024-05-30 21:00:56 +0600

    some style added

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -52,9 +52,9 @@ const Form = () => {
 
   return (
         <form className="flex flex-col gap-3 items-start md:w-[90%] lg:w-[80%] xl:w-[55%] 2xl:w-[50%] ">
-            <input className="border py-2 px-3 rounded-md w-full font-primary" type="text" name="title" value={taskInfo.title} onChange={handleChange} placeholder="Add a title" required/>
-            <textarea className="border rounded-md w-full text-black py-2 px-3 font-primary" rows={10} name="description" value={taskInfo.description} onChange={handleChange} placeholder="Description" required></textarea>
-            <select className="border p-2 rounded-md w-full font-primary" name="category" value={taskInfo.category} onChange={handleChange} required>
+            <input className="bg-white border py-2 px-3 rounded-md w-full font-primary" type="text" name="title" value={taskInfo.title} onChange={handleChange} placeholder="Add a title" required/>
+            <textarea className="bg-white border rounded-md w-full text-black py-2 px-3 font-primary" rows={10} name="description" value={taskInfo.description} onChange={handleChange} placeholder="Description" required></textarea>
+            <select className="bg-white border p-3 rounded-md w-full font-primary" name="category" value={taskInfo.category} onChange={handleChange} required>
                     <option value="">Select a category</option>
                     <option value="Work">Work</option>
                     <option value="Personal">Personal</option>
@@ -68,15 +68,15 @@ const Form = () => {
 
             <div className="flex flex-col gap-1">
                     <label className="font-primary mb-1 font-bold">Priority:</label>
-                    <label className="flex items-center gap-2 font-primary">
+                    <label className="flex items-center gap-2 font-primary  p-1 rounded-md border-2 justify-between bg-white border-green-300">
                         Low
                         <input type="radio" name="priority" value="Low" checked={taskInfo.priority === 'Low'}  onChange={handleChange}/>
                     </label>
-                    <label className="flex items-center gap-2 font-primary">
+                    <label className="flex items-center gap-2 font-primary  p-1 rounded-md border-2 justify-between bg-white border-blue-400">
                         Medium
                         <input type="radio" name="priority" value="Medium" checked={taskInfo.priority === 'Medium'}  onChange={handleChange}/>
                     </label>
-                    <label className="flex items-center gap-2 font-primary">
+                    <label className="flex items-center gap-2 font-primary p-1 rounded-md border-2 justify-between bg-white border-red-500">
                         High
                         <input type="radio" name="priority" value="High" checked={taskInfo.priority === 'High'}  onChange={handleChange}/>
                     </label>
